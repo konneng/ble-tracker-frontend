11506ded177f369782cdeaa8cdca16b183a0479e
konneng <supplierservice@k-eng.it>
2025-07-24 14:49:24 +0200

    Add files via upload

---
+++ main.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+const App = () => (
+  <div>
+    <h1>BLE Tracker Frontend</h1>
+    <p>Login and dashboard coming soon.</p>
+  </div>
+)
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
